117929faef9ad79bf81fecf5e847adcca24e0f41
Nomemb <nkj1205@gmail.com>
2021-10-11 00:34:14 +0900

    test

---
+++ 01Class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * 객체지향
+ * 내가 표현하고 싶은 것을 클래스로 만들어서
+ * 그 클래스로 객체를 만드는 프로그래밍.
+ * 객체를 기반으로 모든 것을 해결함.
+ */
+
+class Inven
+{
+
+}
+
+ class Player
+{
+
+}
+class Pet
+{
+
+}
+class Monster
+{
+
+}
